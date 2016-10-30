792f008d0f60b909de2edc05f8afc380e833c3da
sugir05 <sugirthan05@gmail.com>
2016-10-30 16:50:26 -0500

    angular2-forms-course

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  styles: [`
+    
+  `],
+  template: `
+    <h2>Template Form</h2>
+
+    <template-form></template-form>
+  `
+})
+export class AppComponent {
+  message = 'This is the sample message.';
+}
